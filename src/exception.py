a6baebfce617e598f910ab71c443ecdd0aa0d088
hazimizani <hazimizani@gmail.com>
2026-01-17 10:29:05 -0600

    Finished exceptin and logging and added notebook files

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,13 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
+    # sys.exc_info() returns exception type, exception value, and traceback
     _, _, exc_tb = error_detail.exc_info()
 
+    # .tb_frame: the 'frame' (context) where error happened
+    # .f_code: the code object
+    # .co_filename: the actual filename
     file_name = exc_tb.tb_frame.f_code.co_filename
 
     error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}".format(
@@ -22,5 +26,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
 
